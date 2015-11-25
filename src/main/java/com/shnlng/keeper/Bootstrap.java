b6abc95e2fcc126c10e44fa100cb9e68bb7bdd16
ell <ell.wu@icloud.com>
2015-11-25 20:32:43 +0800

    build assembly

--- src/main/java/com/shnlng/keeper/Bootstrap.java
+++ src/main/java/com/shnlng/keeper/Bootstrap.java
@@ -3,28 +3,45 @@ package com.shnlng.keeper;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+
 public class Bootstrap {
+	public static Logger logger = Logger.getLogger(Bootstrap.class);
+	
 	public static Properties keeperProps = null;
 	public static int keeperServerPort;
 	public static String keeperRepoPath;
 
 	private static void init() {
+		logger.info("Bootstrap init");
 		loadProperties();
 	}
 
 	public static void loadProperties() {
+		String configPath = System.getProperty("config");
+		if(StringUtils.isEmpty(configPath)){
+			configPath = "/keeper.conf";
+			
+		}
+		logger.info("load config file from this path: " + configPath);
+		
 		InputStream is = Bootstrap.class.getClass().getResourceAsStream(
-				"/keeper.properties");
+				configPath);
 		keeperProps = new Properties();
 		try {
 			keeperProps.load(is);
+			logger.info("finish load config file");
 		} catch (Exception e) {
+			logger.error(e.getMessage());
 			e.printStackTrace();
+			logger.info("system exit");
 			System.exit(1);
 		}
 	}
 
 	public static void start() {
+		logger.info("start");
 		init();
 
 		String strPort = keeperProps.getProperty("keeper.server.port");
@@ -37,6 +54,7 @@ public class Bootstrap {
 
 		KeeperServer server = new KeeperServer(keeperServerPort);
 
+		logger.info("run server at " + keeperServerPort);
 		server.run();
 
 	}
---
+++ src/main/scripts/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java -jar ../lib/Keeper-0.0.1-SNAPSHOT.jar
